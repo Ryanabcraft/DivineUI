cd114aef44b4b2497bf579faf9d6c9c8dd7b80ec
Ryanabcraft <ryanab@ryanab.com>
2026-08-31 02:34:02 -0300

    fix: StealAnEgg bump DivineUI to v1.2.1 scroll fix (7a80d6b)

--- steal_an_egg_divineui.lua
+++ steal_an_egg_divineui.lua
@@ -16,7 +16,7 @@ repeat task.wait() until LP.Character and LP.Character:FindFirstChild("HumanoidR
 local IsMobile = UserInputService.TouchEnabled and not UserInputService.MouseEnabled
 
 -- DivineUI
-local DivineUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ryanabcraft/DivineUI/39c8606c36b082ce08de7e6bfd9919767e586884/DivineUI.lua"))()
+local DivineUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ryanabcraft/DivineUI/7a80d6bb1d6c2e2c13dc22020e4ffe45da2006cd/DivineUI.lua"))()
 
 local Window = DivineUI:CreateWindow({
     Title = "Steal an egg",
